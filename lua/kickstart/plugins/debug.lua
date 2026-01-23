5c288769200205612dc2c015e00f6b0ec78339a1
Rodolfo Martins <rodolfomartins88@gmail.com>
2026-01-23 09:17:10 -0300

    Enable Debug for Python

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -94,7 +94,7 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        'debugpy',
       },
     }
 
@@ -135,6 +135,49 @@ return {
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+    -- Configuração do Adaptador
+    dap.adapters.python = function(cb, config)
+      if config.request == 'attach' then
+        local port = (config.connect or config).port
+        local host = (config.connect or config).host or '127.0.0.1'
+        cb {
+          type = 'server',
+          port = assert(port, '`connect.port` é obrigatório para attach'),
+          host = host,
+          options = { source_filetype = 'python' },
+        }
+      else
+        cb {
+          type = 'executable',
+          -- O Mason instala os pacotes neste caminho padrão:
+          command = vim.fn.stdpath 'data' .. '/mason/packages/debugpy/venv/bin/python',
+          args = { '-m', 'debugpy.adapter' },
+          options = { source_filetype = 'python' },
+        }
+      end
+    end
+
+    -- Configuração de como rodar o Python
+    dap.configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = 'Lançar Arquivo Atual',
+        program = '${file}',
+        pythonPath = function()
+          -- Verifica se existe um virtualenv no diretório local
+          local cwd = vim.fn.getcwd()
+          if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+            return cwd .. '/venv/bin/python'
+          elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+            return cwd .. '/.venv/bin/python'
+          else
+            return 'python3' -- Fallback para o python do sistema
+          end
+        end,
+      },
+    }
 
     -- Install golang specific config
     require('dap-go').setup {
